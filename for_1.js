2ecafd921d559495186a8e1bcbc0f05f2b095a03
gulTi <oppok8872l@gmail.com>
2021-08-26 06:36:53 +0800

    first for

---
+++ for_1.js
@@ -0,0 +1,35 @@
+// for-1練習
+// for (i = 1; i<10 ; i++){
+//     for (j=1 ; j<10 ; j++){
+//         console.log(i*j);
+//     } 
+// }
+
+// for-arr練習
+
+// let obj=[
+//     {
+//         month:"1月",
+//         flower:"風信子／水仙／紫羅蘭"
+//     },
+//     {
+//         month:"2月",
+//         flower:"勿忘我／海石竹／櫻草"
+//     },
+//     {
+//         month:"3月",
+//         flower:"矢車菊／雛菊／水仙"
+//     },
+//     {
+//         month:"4月",
+//         flower:"金盞花／山茶花 ／銀蓮花"
+//     },
+//     {
+//         month:"5月",
+//         flower:"蒲公英／歐丁香（洋丁香）／鈴蘭"
+//     },
+// ]
+
+// for (i=0 ; i<obj.length ; i++){
+//     console.log(`出生月份是`+obj[i].month+`的你，代表的花是"`+obj[i].flower+`"`);
+// }
